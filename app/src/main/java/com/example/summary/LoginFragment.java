0d84bc3ce71f7360762f257e564103e6490dd74c
RonnieM <marashdehronnie@gmail.com>
2023-02-23 18:16:51 -0500

    Finalized the front end for login screen.

--- app/src/main/java/com/example/summary/LoginFragment.java
+++ app/src/main/java/com/example/summary/LoginFragment.java
@@ -9,11 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.summary.databinding.FragmentFirstBinding;
+import com.example.summary.databinding.FragmentLoginBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginFragment extends Fragment {
 
-    private FragmentFirstBinding binding;
+    private FragmentLoginBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +22,7 @@ public class LoginFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -32,6 +33,14 @@ public class LoginFragment extends Fragment {
         binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                System.out.println(binding.emailField.getText().toString());
+                System.out.println(binding.passwordField.getText().toString());
+                if (!binding.emailField.getText().toString().equals("test@test.com") || !binding.passwordField.getText().toString().equals("password")) {
+                    binding.incorrect.setText("Incorrect username or password. Please try again.");
+                    binding.passwordField.setText("");
+                    binding.emailField.setText("");
+                    return;
+                }
                 NavHostFragment.findNavController(LoginFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
